b7b713ca1600a1c6ac39e57b55c6fae7d6431c14
TuTuHuss <hushuai001@163.com>
2024-09-11 20:55:47 +0800

    polish(hus): add 2 new model's result in csv

--- data/make_csv.py
+++ data/make_csv.py
@@ -5,7 +5,7 @@ import csv
 def main():
     base_dir = '.'
     csv_file = 'output_0911_n3.csv'
-    columns = ['编号', '标签', '题目概述', '输入', '输出（deepseekv2_5）', '输出（gpt_4o_20240513ptu）', '输出（qwen2_72b_instruct）']
+    columns = ['编号', '标签', '题目概述', '输入', '输出（deepseekv2_5）', '输出（gpt_4o_20240513ptu）', '输出（qwen2_72b_instruct）', '输出（ours_0829_pretrain_full）', '输出（ours_0829_pretrain_half）']
     #columns = ['编号', '标签', '题目概述', '输入', '输出（deepseekv2_5）', '输出（gpt_4o_20240513ptu）', '输出（qwen2_72b_instruct）', '指令（instruct）输入']
 
     with open(csv_file, 'w', newline='', encoding='utf-8') as f:
@@ -31,6 +31,8 @@ def main():
         deepseek_file = os.path.join(completion_path, 'output_deepseekv2_5.txt')
         gpt_file = os.path.join(completion_path, 'output_gpt_4o_20240513ptu.txt')
         qwen_file = os.path.join(completion_path, 'output_qwen2_72b_instruct.txt')
+        ours_0829_pretrain_full_file = os.path.join(completion_path, "output_internlm25_fullpt_0829_2100M_filterd_codemath_360M_fullprompt.txt")
+        ours_0829_pretrain_half_file = os.path.join(completion_path,"output_internlm25_fullpt_0829_2100M_filterd_codemath_360M_halfprompt.txt")
 
         with open(input_file, 'r', encoding='utf-8') as f:
             input_text = f.read().strip()
@@ -43,6 +45,13 @@ def main():
 
         with open(qwen_file, 'r', encoding='utf-8') as f:
             qwen_output = f.read().strip()
+        
+        with open(ours_0829_pretrain_full_file, 'r', encoding='utf-8') as f:
+            ours_0829_pretrain_full_output = f.read().strip()
+
+        with open(ours_0829_pretrain_half_file, 'r', encoding='utf-8') as f:
+            ours_0829_pretrain_half_output = f.read().strip()
+
 
         row = {
             '编号': number,
@@ -52,6 +61,8 @@ def main():
             '输出（deepseekv2_5）': deepseek_output,
             '输出（gpt_4o_20240513ptu）': gpt_output,
             '输出（qwen2_72b_instruct）': qwen_output,
+            '输出（ours_0829_pretrain_full）': ours_0829_pretrain_full_output,
+            '输出（ours_0829_pretrain_half）': ours_0829_pretrain_half_output
             #'指令（instruct）输入': instruct,
         }
 
